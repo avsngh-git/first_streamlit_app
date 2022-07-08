64d261502ee37c0be62e4566fcadbc605552a29a
avsngh-git <67750197+avsngh-git@users.noreply.github.com>
2022-07-08 12:20:00 +0530

    Update streamlit_app.py
    added some things

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 import pandas
-streamlit.title('hello this is my first app')
+streamlit.title("Daddy's Breakfast Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text('Oatmeal')
 streamlit.text('Banana smoothie')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.header('🍋🍇List of Fruits Available🍉🥭'
 streamlit.dataframe(my_fruit_list)
