f5c7e901bb8d3a724ee548d0c10dc27ba04357de
avsngh-git <67750197+avsngh-git@users.noreply.github.com>
2022-07-08 12:24:09 +0530

    Update streamlit_app.py
    added some comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Oatmeal')
 streamlit.text('Banana smoothie')
 
+
+# Read CSV from S3 bucket and display the dataframe on the app
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.header('🍋🍇List of Fruits Available🍉🥭')
 streamlit.dataframe(my_fruit_list)
