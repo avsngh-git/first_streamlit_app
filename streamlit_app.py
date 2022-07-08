f927741c0f31aff39cba4fa500502c50e0903ac8
avsngh-git <67750197+avsngh-git@users.noreply.github.com>
2022-07-08 12:16:40 +0530

    Update streamlit_app.py
    added fruit dataframe from s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
-import streamlit 
+import streamlit
+import pandas
 streamlit.title('hello this is my first app')
 streamlit.header('Breakfast Menu')
 streamlit.text('Oatmeal')
 streamlit.text('Banana smoothie')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
