ca750ca7c03132a72cc9c7a47b2b6ba578efa599
avsngh-git <67750197+avsngh-git@users.noreply.github.com>
2022-07-08 12:41:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ streamlit.header('🍋🍇List of Fruits Available🍉🥭')
 # Allow customers to pick a list of fruits 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc(fruits_selected)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the dataframe
 streamlit.dataframe(fruits_to_show)
